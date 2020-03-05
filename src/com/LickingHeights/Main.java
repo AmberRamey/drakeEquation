05100a0c5588e030b1ec599c734701cf413b1d81
Amber <amber.ramey.2022@student.lhschools.org>
2020-03-05 11:09:08 -0500

    f_c input added

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -53,6 +53,9 @@ public class Main {
         System.out.println("What is the fraction of that life that develops intelligence?");
         f_i = Drake.nextInt();
 
+        System.out.println("What is the fraction that develops detectable communications?");
+        f_c = Drake.nextInt();
+
     N = rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L;
 
 
